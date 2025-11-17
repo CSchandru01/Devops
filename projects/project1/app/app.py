c524263fdbb8cb317b72f187f1020ebd946691a1
Chandrashekhar Mahantesh Mahadum <128290016+CSchandru01@users.noreply.github.com>
2025-11-17 06:31:57 +0000

    Development: app.py modified

--- projects/project1/app/app.py
+++ projects/project1/app/app.py
@@ -2,13 +2,26 @@
 
 from flask import Flask
 import socket
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Project 1!"  
+    env_var = os.getenv('APP_MESSAGE', 'DEFAULT MESSAGE')
+    secret_message = os.getenv('SECRET_TOKEN', 'NO SECRET')
+    host=socket.gethostname()
+    return f"{enc_msg} |Secret: {secret_message} | Host: {host}"
+
+@app.route('/health')
+def health():
+    return "OK", 200   
+
+@app.route('/ready')
+def ready():
+    return "READY", 200  
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
 
